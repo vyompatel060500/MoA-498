07294114ee068f8c2045843115dcdb2d23aa6b46
Vyom Patel <vyompatel060500@yahoo.com>
2020-10-28 18:22:03 -0600

    Made quite a few changes in logistic.r mainly in the incorrect application of foreach loops in previous commits. Hopefully this settles it up.

--- logistic.r
+++ logistic.r
@@ -4,11 +4,8 @@ library(MASS)
 library(boot)
 library(speedglm)
 library(readr)
-
 library(foreach)
 library(doParallel)
-cl<-makeCluster(20)     # modify this to number of parallel processes required
-registerDoParallel(cl)
 
 
 logloss<-function(predicted, actual)
@@ -42,38 +39,49 @@ test_y<-train_scores[-train,]%>% dplyr::select(-sig_id)
 t<-test_y %>% dplyr::select(nfkb_inhibitor) 
  
 
-
 predictors = names(train_y)
 models = list()
 loglosses = list()
 
+cl<-makeCluster(4)     # modify this to number of parallel processes required
+registerDoParallel(cl)
 
-
-foreach(i=1:length(predictors)  ,.packages=c("glue","dplyr","speedglm")) %dopar% {
-  
+start_time<-Sys.time()
+print(glue("Started training models..."))
+# The foreach loop trains models for each of the output variables and returns a list of
+# models. To access i'th model, use models[[i]].
+models<-foreach(i=1:length(predictors)  ,.packages=c("glue","dplyr","speedglm")) %dopar% {
   train_y_predictor<-train_y %>% dplyr::select(predictors[i]) %>% unlist(use.names = FALSE)
-  test_y_predictor<-test_y %>% dplyr::select(predictors[i]) %>% unlist(use.names = FALSE)
-  
-  
-  models[[predictors[i]]]<-speedglm(train_y_predictor~.,data = data.frame(train_x),family = binomial(),maxit = 200)
-  pred = predict(models[[predictors[i]]],newdata = test_x,type="response")
-  
-  loss<-logloss(pred,test_y_predictor)
-  loglosses[predictors[i]] = loss
-  
-  print(glue("The logloss for {predictors[i]} on test data was {loss}"))
+  speedglm(train_y_predictor~ . ,data = data.frame(train_x), family=binomial(), maxit = 250)
+}
+end_time<-Sys.time()
+diff=difftime(end_time,start_time,units="secs")
+print(glue("Time taken for training models: {diff} seconds."))
+
+print(glue("Started prediction on trained models..."))
+# This foreach loop predicts the probabilities using trained models foe each output
+# variable and returns a list of predictions. To access 'ith model's prediction,
+# use preds[[i]]
+preds<-foreach(i=1:length(predictors)  ,.packages=c("glue","dplyr","speedglm")) %dopar% {
+  predict(models[[i]],newdata = test_x,type="response")
 }
 
-total = 0
-for (loss in loglosses){total = total+loss}
-print(glue("The mean(final) logloss of the model is: {total/length(loglosses)}."))
+print(glue("Computing loglosses of predicted outcomes..."))
+# This foreach loop computes the logloss value for each output variable using
+# the predictions generated from each model. To access i'th variable's logloss
+# use loglosses[[i]]
+loglosses<-foreach(i=1:length(predictors)  ,.packages=c("glue","dplyr","speedglm")) %dopar% {
+  test_y_predictor<-test_y %>% dplyr::select(predictors[i]) %>% unlist(use.names = FALSE)
+  logloss(preds[[i]],test_y_predictor)
+}
+
+print(glue("The mean(final) logloss of the model is: {mean(unlist(loglosses))}."))
  
 
 submission=list()
-for(i in (predictors)){
+for(i in 1:length(predictors)){
   submission[[i]] = predict(models[[i]] , newdata = test_features,type="response")
 }
-
 write_csv(data.frame(submission), 'submission1.csv')
  
 
