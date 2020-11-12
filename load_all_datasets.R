538212d1b2f3b2183d0073043a69309f87a1c005
Aaron Toderash (adt150) <aaron.t@usask.ca>
2020-11-12 01:29:03 -0600

    Aaron: minor updates

--- load_all_datasets.R
+++ load_all_datasets.R
@@ -4,7 +4,7 @@ library(readr)
 
 path_why <- ""
 
-train_features <- read_csv(glue("{path_why}lish-moa/train_features.csv")) %>% fix_names
-train_scores <- read_csv(glue("{path_why}lish-moa/train_targets_scored.csv")) %>% fix_names
-test_features <- read_csv(glue("{path_why}lish-moa/test_features.csv")) %>% fix_names
-train_drug    <- read_csv(glue("{path_why}lish-moa/train_drug.csv")) %>% fix_names
+train_features <- read_csv(glue("{path_why}lish-moa/train_features.csv"))     # %>% fix_names
+train_scores <- read_csv(glue("{path_why}lish-moa/train_targets_scored.csv")) # %>% fix_names
+test_features <- read_csv(glue("{path_why}lish-moa/test_features.csv"))       # %>% fix_names
+train_drug    <- read_csv(glue("{path_why}lish-moa/train_drug.csv"))          # %>% fix_names
