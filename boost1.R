69b8c3e148169dd737c63d12f2a1c45d30b35a7b
Vyom Patel <vyompatel060500@yahoo.com>
2020-11-30 20:50:58 -0600

    Commented version of boost1.R

--- boost1.R
+++ boost1.R
@@ -1,7 +1,7 @@
 #!/bin/Rscript
 print("Starting...")
 library(glue)
-library(tidyverse) 
+library(tidyverse)
 library(MASS)
 library(boot)
 library(speedglm)
@@ -15,38 +15,23 @@ library(onehot)
 #$//$ Define functions here: $//$
 
 
-logloss<-function(predicted, actual)
+logloss <- function(predicted, actual)
 {   #function to compute the Log-Loss
   
   # :param : actual- Ground truth (correct) 0-1 labels vector
   # :param : predicted- predicted values from the model
-  # return: result- log-loss value
+  # :return : result- log-loss value
   result<- -1/length(actual)*(sum((actual*log(predicted)+(1-actual)*log(1-predicted))))
   return(result)
 }
-get_best_result = function(caret_fit) {
-  best = which(rownames(caret_fit$results) == rownames(caret_fit$bestTune))
-  best_result = caret_fit$results[best, ]
-  rownames(best_result) = NULL
-  best_result
-}
-important_features<-function(features, threshold){
-  # returns all predictors with correlation less than threshold
-  corr_matrix<-cor(features)
-  columns<-rep(TRUE,nrow(corr_matrix))
-  
-  for(i in 1:length(columns) - 1){
-    for(j in (i+1):length(columns)){
-      if( length(corr_matrix[i,j]) > 0 && abs(corr_matrix[i,j])>= threshold){
-        columns[j]<-FALSE
-      }
-    }
-  }
-  
-  return (colnames(features)[columns])
-}
 
 convert_onehot<-function(x){
+  # function to convert the categorical features to
+  # one-hot encoding
+
+  # :param : x - input data.frame containing categorical features
+  # :return : temp_onehot : a data.frame containing all the input features
+  #                         converted to one-hot encoding 
   input<-x
   trt_cnd = c('cp_type', 'cp_time', 'cp_dose')
   cp<-input[,trt_cnd]
@@ -57,34 +42,31 @@ convert_onehot<-function(x){
 }
 
 
-fix_names <- function(df) {
-  names(df) <- gsub('-', '_', names(df))
-  df
-}
-
-# path_why <- "./project498/MoA-498/"
 path_why <- "/home/patel/project498/MoA-498/"
 
+# Read the data
 train_features <- read_csv(glue("{path_why}lish-moa/train_features.csv")) 
 train_scores <- read_csv(glue("{path_why}lish-moa/train_targets_scored.csv"))
 test_features_input <- read_csv(glue("{path_why}lish-moa/test_features.csv"))
 sample_submission<-read_csv(glue("{path_why}lish-moa/sample_submission.csv"))
-tSNE<-read_csv(glue("{path_why}lish-moa/tsne4dims.csv"))
 
+# Train-test split
 set.seed(498)
 test = sample(1:nrow(train_features), nrow(train_features)/10)
 train = -test
+
 train_y<-train_scores[train,] %>% dplyr::select(-sig_id)
 predictors = names(train_y)
 num_cols_to_use = length(predictors)
+
+#Grab sif_ids of train and test data sets
 test_x_sig_id<-train_features[test,] %>% dplyr::select(sig_id)
 test_features_sig_id<-test_features_input %>% dplyr::select(sig_id)
 
+# store train and test datasets in separate variables after dropping the sig_ids. 
 train_x<-train_features[train,] %>% dplyr::mutate(cp_type = factor(cp_type), cp_dose = factor(cp_dose), cp_time = factor(cp_time)) %>%dplyr::select(-sig_id)
 test_x<-train_features[test,] %>% dplyr::mutate(cp_type = factor(cp_type), cp_dose = factor(cp_dose), cp_time = factor(cp_time)) %>% dplyr::select(-sig_id)
 test_features<-test_features_input %>% dplyr::mutate(cp_type = factor(cp_type), cp_dose = factor(cp_dose), cp_time = factor(cp_time)) %>% dplyr::select(-sig_id)
-#tSNE_train<-tSNE[train,]
-#tSNE_test<-tSNE[test,]
 test_y<-train_scores[test,]%>% dplyr::select(-sig_id)
 
 
@@ -93,11 +75,12 @@ train_x_onehot<-convert_onehot(train_x)
 test_x_onehot<-convert_onehot(test_x)
 test_features_onehot<-convert_onehot(test_features)
 
+# index of all data points which are ctl_vechicles
 train_not_ctl = train_x_onehot$type_ctl != 1
 test_not_ctl = test_x_onehot$type_ctl != 1
 test_features_not_ctl = test_features_onehot$type_ctl != 1
 
-
+#Separate dataset into g and c features
 train_x_g<-train_x%>%dplyr::select(starts_with('g-'))
 train_x_c<-train_x%>%dplyr::select(starts_with('c-'))
 test_x_g<-test_x%>%dplyr::select(starts_with('g-'))
@@ -106,6 +89,7 @@ test_feat_g<-test_features%>%dplyr::select(starts_with('g-'))
 test_feat_c<-test_features%>%dplyr::select(starts_with('c-'))
 
 
+#Perform PCA#
 print(glue("Starting PCA..."))
 pca_g = preProcess(train_x_g, method = 'pca', thresh = 0.80)
 pca_c = preProcess(train_x_c, method = 'pca', thresh = 0.90)
@@ -117,20 +101,24 @@ test_feat_g<-predict(pca_g, test_feat_g)
 test_feat_c<-predict(pca_c, test_feat_c)
 print(glue("Completed PCA!"))
 
+# Modifiy colnames of train and test dataset to avoid conlflicts
 names(train_x_g)<-glue("PCg-{c(1:length(train_x_g))}")
 names(test_x_g)<-glue("PCg-{c(1:length(test_x_g))}")
 names(test_feat_g)<-glue("PCg-{c(1:length(test_feat_g))}")
 
+# combine all features of train-test dataset after pre-processing into corresponding variable
 train_x_all<-(cbind(train_x_onehot, train_x_g, train_x_c) %>% as_tibble())[train_not_ctl,-c(1,2)]
 test_x_all<-(cbind(test_x_onehot, test_x_g, test_x_c) %>% as_tibble())[,-c(1,2)]
 test_features_all<-(cbind(test_features_onehot, test_feat_g, test_feat_c) %>% as_tibble())[,-c(1,2)]
 
 
-cl<-makeCluster(8)
+# Initialize clusters to perform parallel model training
+cl<-makeCluster(8)          # Change this to number of cores/parallel models to train. CAREFUL!!
 registerDoParallel(cl)
 start_time<-Sys.time()
 print(glue("Started training models..."))
 
+#Start Model Training
 models<-foreach(i=1:length(predictors)  ,.packages=c("glue","dplyr","xgboost")) %dopar% {
   train_y_predictor<-train_y[train_not_ctl,] %>% dplyr::select(predictors[i]) %>% unlist(use.names = FALSE)
   datamatrix<-xgb.DMatrix(data = as.matrix(train_x_all), label = train_y_predictor)
@@ -143,27 +131,31 @@ print(glue("Training Complete!"))
 print(glue("Time taken for training models: {diff} seconds."))
 stopCluster(cl)
 
-
+# Predict on test data
 print(glue("Starting predictions on test data..."))
 test_preds<-foreach(i=1:num_cols_to_use  ,.packages=c("glue","dplyr","xgboost")) %do% {
   pred<-predict(models[[i]],newdata = as.matrix(test_x_all))
 }
 print(glue("Prediction complete!\n"))
 
+# Add 0's to all data points which were ctl_vehicles
 for(i in 1:length(test_preds)){
   test_preds[[i]][!test_not_ctl] = 0
 }
 
+#Predict on train data
 print(glue("Starting predictions on train data..."))
 train_preds<-foreach(i=1:num_cols_to_use  ,.packages=c("glue","dplyr","xgboost")) %do% {
   pred<-predict(models[[i]],newdata = as.matrix(train_x_all ))
 }
 print(glue("Prediction complete!\n"))
 
+# Add 0's to ctl_vechicles
 for(i in 1:length(train_preds)){
   train_preds[[i]][!train_not_ctl] = 0
 }
 
+# Calculate the log-loss score
 print(glue("Starting logloss calculation..."))
 loglosses<-foreach(i=1:num_cols_to_use  ,.packages=c("glue","dplyr","xgboost")) %do% {
   test_y_predictor<-test_y %>% dplyr::select(predictors[i]) %>% unlist(use.names = FALSE)
@@ -180,7 +172,7 @@ train_loglosses<-foreach(i=1:num_cols_to_use  ,.packages=c("glue","dplyr","xgboo
 print(glue("Logloss on test data: {mean(loglosses%>%unlist())}\n"))
 print(glue("Logloss on train data: {mean(train_loglosses%>%unlist())}\n"))
 
-
+#Generate the final submission file for Kaggle
 for(i in 1:length(predictors)){
   pred = predict(models[[i]] , newdata = as.matrix(test_features_all))
   pred[!test_features_not_ctl] = 0
